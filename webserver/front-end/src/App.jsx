a98ca1c4cbd351110e193efd9bbdfa50066d0191
zhulei <zhulei@xmfunny.com>
2025-04-14 16:50:01 +0800

    <feature> add welcome

--- webserver/front-end/src/App.jsx
+++ webserver/front-end/src/App.jsx
@@ -1,10 +1,11 @@
 import './css/App.css'
 import Footer from './components/Footer'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="container">
-      <h1 className="rainbow-text">你好! Sofunny 3D</h1>
+      <Welcome />
       <Footer />
     </div>
   )
---
+++ webserver/front-end/src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import '../css/Welcome.css'
+
+function Welcome() {
+    return (
+        <div className="container">
+            <h1 className="rainbow-text">你好! Sofunny 3D</h1>
+        </div>
+    )
+}
+
+export default Welcome
