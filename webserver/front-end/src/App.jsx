d90fc7c29e0fb82ddf50a30abd67645b7b32c038
zhulei <zhulei@xmfunny.com>
2025-04-14 16:12:43 +0800

    <feature> rm useless

--- webserver/front-end/src/App.jsx
+++ webserver/front-end/src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="container">
       <h1 className="rainbow-text">你好! Sofunny 3D</h1>
