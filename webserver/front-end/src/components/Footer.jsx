a9f8c9f70f13878103284a9e1542931d3e386306
zhulei <zhulei@xmfunny.com>
2025-04-14 16:44:58 +0800

    <feature> add unreal link

--- webserver/front-end/src/components/Footer.jsx
+++ webserver/front-end/src/components/Footer.jsx
@@ -5,13 +5,16 @@ function Footer() {
         <div className="container">
             <footer className="footer">
                 <a href="https://kenney.nl/assets" target="_blank" rel="noopener noreferrer">
-                    <img src="https://kenney.nl/favicon.ico" alt="Example 1" className="footer-icon" />
+                    <img src="https://kenney.nl/favicon.ico" alt="kenney assets" className="footer-icon" />
                 </a>
                 <a href="https://assetstore.unity.com/" target="_blank" rel="noopener noreferrer">
-                    <img src="https://assetstore.unity.com/favicon.ico" alt="Example 2" className="footer-icon" />
+                    <img src="https://assetstore.unity.com/favicon.ico" alt="unity assets store" className="footer-icon" />
                 </a>
                 <a href="https://sketchfab.com/" target="_blank" rel="noopener noreferrer">
-                    <img src="https://sketchfab.com/favicon.ico" alt="Example 3" className="footer-icon" />
+                    <img src="https://sketchfab.com/favicon.ico" alt="sketch fab" className="footer-icon" />
+                </a>
+                <a href="https://www.fab.com/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://static.fab.com/static/builds/web/dist/frontend/assets/images/common/favicon/0474bf3a774e077cf48b0c755890d7ed-v1.svg" alt="unreal market place" className="footer-icon" />
                 </a>
             </footer>
         </div>
@@ -19,3 +22,4 @@ function Footer() {
 }
 
 export default Footer
+
