b9ccffdb143baba159763699154d047ac94e86e8
zhulei <zhulei@xmfunny.com>
2025-04-15 13:52:10 +0800

    <feature> multi pages supported

--- webserver/front-end/src/main.jsx
+++ webserver/front-end/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './css/index.css'
 import App from './components/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
