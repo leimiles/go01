84b31ab26c38c4358eceaac871546190d1961c53
汤玥 <tangyue@xmfunny.com>
2025-05-12 10:06:26 +0800

    <feature>对Z-up模型进行旋转矫正

--- webserver/front-end/src/models/Model.js
+++ webserver/front-end/src/models/Model.js
@@ -3,6 +3,8 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { TGALoader } from 'three/examples/jsm/loaders/TGALoader';
 
+import * as THREE from 'three'
+
 export default class Model {
     constructor() {
         this.modelRoot = null
@@ -28,6 +30,8 @@ export default class Model {
         loader.manager.addHandler(/\.tga$/i, new TGALoader())
         return new Promise((resolve, reject) => {
             loader.load(url, (object) => {
+                // 如果是 Z-up 模型，则矫正旋转值
+                this.fixModelRotation(object)
                 // 如果没有贴图或材质就给一个默认的灰色材质
                 this.fixMaterials(object)
                 this.modelRoot = object
@@ -49,6 +53,29 @@ export default class Model {
                 resolve(clips)
             }, undefined, reject)
         })
+    }
+
+    // 对 Z-up 模型进行旋转矫正
+    fixModelRotation(object) {
+        // 寻找根骨骼
+        let rootBone = null
+        object.traverse(child => {
+            if (child.isBone && !child.parent?.isBone) {
+                rootBone = child
+            }
+        })
+
+        // 如果存在根骨骼且Z轴旋转90度
+        if (rootBone) {
+            const worldEuler = new THREE.Euler().setFromQuaternion(
+                rootBone.getWorldQuaternion(new THREE.Quaternion())
+            )
+            const zDeg = Math.abs(THREE.MathUtils.radToDeg(worldEuler.z))
+            if (Math.abs(zDeg - 90) < 1) {
+                object.rotation.set(-Math.PI / 2, 0, 0)
+                object.updateMatrixWorld(true);
+            }
+        }
     }
 
     // 修复材质
