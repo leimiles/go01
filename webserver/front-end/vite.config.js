bbd476998e61277c8c53bb99bed701484941d28e
汤玥 <tangyue@xmfunny.com>
2025-05-16 16:05:13 +0800

    <feature>vite代理转发配置

--- webserver/front-end/vite.config.js
+++ webserver/front-end/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/assets-lib': {
+        target: 'https://webgl.sofunny.io',
+        changeOrigin: true
+      }
+    }
+  }
 })
